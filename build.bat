8aae9c0228ff71a0f258e0c76f539c5cdbbff8cf
Arvid Gerstmann <dev@arvid-g.de>
2017-08-04 08:06:54 +0200

    Update for use with Visual Studio 2017

--- build.bat
+++ build.bat
@@ -3,8 +3,9 @@
 
 :: Change to your LLVM installation
 set "LLVMPath=C:\Program Files\LLVM"
-:: Change to your Visual Studio 2015 installation
-set "VSPath=C:\Program Files (x86)\Microsoft Visual Studio 14.0"
+:: Change to your Visual Studio 2017 installation
+set "VSPath=C:\Program Files (x86)\Microsoft Visual Studio\2017\Community"
+set "VSVersion=14.10.25017"
 :: Change to your Windows Kit version & installation
 set "WinSDKVersion=10.0.15063.0"
 set "WinSDKPath=C:\Program Files (x86)\Windows Kits\10"
@@ -13,17 +14,18 @@ set "OUTPUT=test.exe"
 
 
 :: Setup
-set "PATH=%PATH%;%LLVMPath%\bin;%VSPath%\VC\bin\amd64"
+set "VSBasePath=%VSPath%\VC\Tools\MSVC\%VSVersion%"
+set "PATH=%PATH%;%LLVMPath%\bin;%VSBasePath%\bin\HostX64\x64"
 
 :: Compiler Flags
 set CFLAGS= ^
  -std=c++14 -Wall -Wextra
 
 set CPPFLAGS= ^
-  -I "%VSPath%\include" ^
-  -I "%WinSDKPath%\Include/%WinSDKVersion%\shared" ^
-  -I "%WinSDKPath%\Include/%WinSDKVersion%\ucrt" ^
-  -I "%WinSDKPath%\Include/%WinSDKVersion%\um"
+  -I "%VSBasePath%\include" ^
+  -I "%WinSDKPath%\Include\%WinSDKVersion%\shared" ^
+  -I "%WinSDKPath%\Include\%WinSDKVersion%\ucrt" ^
+  -I "%WinSDKPath%\Include\%WinSDKVersion%\um"
 
 
 :: Linker Libs
@@ -33,7 +35,7 @@ set LDFLAGS= ^
  -subsystem:console
 
 set LDLIBS= ^
- -libpath:"%VSPath%\VC\lib\amd64" ^
+ -libpath:"%VSBasePath%\lib\x64" ^
  -libpath:"%WinSDKPath%\Lib\%WinSDKVersion%\ucrt\x64" ^
  -libpath:"%WinSDKPath%\Lib\%WinSDKVersion%\um\x64" ^
  libucrt.lib libvcruntime.lib libcmt.lib libcpmt.lib ^
@@ -42,18 +44,10 @@ set LDLIBS= ^
  kernel32.lib User32.lib
 
 
-:: Required Microsoft extensions
-set MSEXT= ^
- -fms-compatibility -fdelayed-template-parsing ^
- -fms-compatibility-version=19.10.25019 ^
- -fms-extensions -fvisibility-ms-compat ^
- -mms-bitfields
-
-
 :: Compiling
 @echo on
 @for %%f in (*.cc) do (
-    clang++.exe "%%~f" -o "%%~nf.o" -c %CFLAGS% %MSEXT%
+    clang++.exe "%%~f" -o "%%~nf.o" -c %CFLAGS%
 )
 
 :: Linking
@@ -63,5 +57,4 @@ set MSEXT= ^
 )
 
 lld-link.exe %LINK_FILES% -out:"%OUTPUT%" %LDFLAGS% %LDLIBS%
-
 
